99e8182f6a921ef5c041550373ab43b458f5be10
Khaldon Naif <khaldonnaif@gmail.com>
2025-06-05 23:53:11 +0800

    Add results and filter

--- index.js
+++ index.js
@@ -11,12 +11,32 @@ app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.render("home.ejs");
+});
+
+app.post("/result", async (req, res) => {;
+  try {
+    const response = await axios.get(`https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&latitude=${req.body.latitude}&longitude=${req.body.longitude}&maxradiuskm=${req.body.radius}`);
+    res.render("result.ejs", { data: response.data });
+  } catch(error) {
+    res.send("Error upon sending an API request. Make sure the coordinates you sent are valid.");
+  }
+});
+
+app.post("/filter", async (req, res) => {
+  const url = req.body.url;
+  const filterLevel = req.body.minimumMagnitude;
+  try {
+    const response = await axios.get(`${url}`);
+    res.render("result.ejs", {data: response.data, filterLevel: filterLevel});
+  } catch(error) {
+    res.sendStatus(500).json(error);
+  }
 })
 
 app.get("/about", (req, res) => {
   res.render("about.ejs");
-})
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
-})
+});
